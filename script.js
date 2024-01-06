bfabd047a1fe71440e9d451c3ec95f5bea23c542
Santiago Tovar <santiagocentenot@gmail.com>
2024-01-06 09:37:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const d = document,
+  w = window;
+
+let $li = d.querySelectorAll(".faq-text li");
+
+for (var i = 0; i < $li.length; i++) {
+  $li[i].addEventListener("click", (e) => {
+    let clickedLi;
+    if (e.target.classList.contains("question-arrow")) {
+      clickedLi = e.target.parentElement;
+    } else {
+      clickedLi = e.target.parentElement.parentElement;
+    }
+    clickedLi.classList.toggle("showAnswer");
+  });
+}
